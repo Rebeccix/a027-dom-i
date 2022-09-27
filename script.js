4ae44374fbf64325b58eab88266c397dcb8ef1fb
Rebecca Ferreira <beccixzin@gmail.com>
2022-09-26 22:44:03 -0300

    finalizado

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 const frutas = ["laranja", "limão", "uva"];
+
+const firstLi = document.getElementById("fruta-1")
+ 
+firstLi.innerHTML = frutas[0]
+
+const secondLi = document.getElementById("fruta-2")
+ 
+secondLi.innerHTML = frutas[1]
+
+const thirdLi = document.getElementById("fruta-3")
+ 
+thirdLi.innerHTML = frutas[2]
+
+const fourthLi = document.getElementById("fruta-4")
+
+const inputFruta = document.getElementById("fruta-input")
+
+const clicar = () => {
+    fourthLi.innerHTML = inputFruta.value
+}
